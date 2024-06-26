08aac56ddd36637716d19d8ccdbaff415ea335d5
Yasir Khan <khanyaser007@gmail.com>
2024-06-26 23:49:10 +0800

    Add button link and hero components

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,31 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface BlocksHero extends Schema.Component {
+  collectionName: 'components_blocks_heroes';
+  info: {
+    displayName: 'Hero';
+  };
+  attributes: {
+    heading: Attribute.String;
+    text: Attribute.Text;
+    link: Attribute.Component<'elements.button-link'>;
+    image: Attribute.Media<'images'>;
+  };
+}
+
+export interface ElementsButtonLink extends Schema.Component {
+  collectionName: 'components_elements_button_links';
+  info: {
+    displayName: 'Button Link';
+  };
+  attributes: {
+    title: Attribute.String;
+    link: Attribute.String;
+    isExternal: Attribute.Boolean & Attribute.DefaultTo<false>;
+    type: Attribute.Enumeration<['PRIMARY', 'SECONDARY']>;
+  };
+}
+
 export interface PostsSocialAutomation extends Schema.Component {
   collectionName: 'components_posts_social_automations';
   info: {
@@ -15,6 +41,8 @@ export interface PostsSocialAutomation extends Schema.Component {
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
+      'blocks.hero': BlocksHero;
+      'elements.button-link': ElementsButtonLink;
       'posts.social-automation': PostsSocialAutomation;
     }
   }
--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -378,6 +378,7 @@ export interface ApiLandingPageLandingPage extends Schema.CollectionType {
     description: Attribute.Text;
     slug: Attribute.UID<'api::landing-page.landing-page', 'title'>;
     metadata: Attribute.Component<'posts.social-automation'>;
+    blocks: Attribute.DynamicZone<['blocks.hero']>;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
